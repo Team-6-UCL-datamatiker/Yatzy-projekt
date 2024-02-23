006935ecfed4033e2a039387361362711ff2baed
Jeppe Lynge Hansen <jklh62358@edu.ucl.dk>
2024-02-23 14:32:06 +0100

    Add project files.

---
+++ DieTest/Die.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieTest
+{
+    internal class Die
+    {
+        private int value;
+        private bool isLocked;
+
+        public void roll()
+        {
+            if (this.isLocked == false)
+            {
+                Random r = new Random();
+                int randomValue = r.Next(1, 7);
+                this.value = randomValue;
+            }
+
+        }
+
+        public int getValue()
+        {
+            return this.value;
+        }
+
+        public void freeze()
+        {
+            this.isLocked = true;
+        }
+
+        public void unlock()
+        {
+            this.isLocked = false;
+        }
+    }
+}
---
+++ DieTest/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DieTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die1 = new Die();
+
+            int index = 0;
+            while (index < 100)
+            {
+                die1.roll();
+                //die1.freeze();
+                Console.WriteLine(die1.getValue());
+                index++;
+            }
+        }
+    }
+}
