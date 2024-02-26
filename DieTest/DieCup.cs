e33257f6944c0c5eb40f6a22ab6fc5d8993ed5c5
Jeppe Lynge Hansen <jklh62358@edu.ucl.dk>
2024-02-26 11:17:54 +0100

    Added DieCup class and tested it in the main program

---
+++ DieTest/DieCup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieTest
+{
+    internal class DieCup
+    {
+        private Die[] dice = new Die[5];
+
+        private Die die1 = new Die();
+        private Die die2 = new Die();
+        private Die die3 = new Die();
+        private Die die4 = new Die();
+        private Die die5 = new Die();
+
+        public DieCup()
+        {
+            dice[0] = die1;
+            dice[1] = die2;
+            dice[2] = die3;
+            dice[3] = die4;
+            dice[4] = die5;
+        }
+
+        public void roll()
+        {
+            foreach (Die d in dice)
+            {
+                d.roll();
+            }
+        }
+
+        public void lockDie(int index)
+        {
+            if (index >= 0 && index < dice.Length)
+            {
+                dice[index].freeze();
+            }
+        }
+
+        public void unlockDie(int index)
+        {
+            if (index >= 0 && index < dice.Length)
+            {
+                dice[index].unlock();
+            }
+        }
+
+        public void unlockAllDice()
+        {
+            foreach (Die d in dice)
+            {
+                d.unlock();
+            }
+        }
+
+        public void printResults()
+        {
+            foreach (Die d in dice)
+            {
+                Console.WriteLine(d.getValue());
+            }
+        }
+    }
+}
--- DieTest/Program.cs
+++ DieTest/Program.cs
@@ -4,15 +4,74 @@
     {
         static void Main(string[] args)
         {
-            Die die1 = new Die();
+            //test program
+            DieCup dieCup = new DieCup();
 
-            int index = 0;
-            while (index < 100)
+            int rollCount = 0;
+            while (rollCount < 3)
             {
-                die1.roll();
-                //die1.freeze();
-                Console.WriteLine(die1.getValue());
-                index++;
+
+                Console.WriteLine("Rolls left: " + (3 - rollCount));
+                Console.WriteLine("Press 1 to roll");
+                while (true)
+                {
+                    try //handle crash for wron inputs
+                    {
+                        int input = int.Parse(Console.ReadLine());
+                        Console.WriteLine("---------------");
+                        if (input == 1) break;
+                        else Console.WriteLine("Please press 1");
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine("---------------");
+                        Console.WriteLine("Please press 1");
+                    }
+
+                }
+
+                dieCup.roll();
+                dieCup.printResults();
+                rollCount++;
+                Console.WriteLine("---------------");
+
+                //Freeze part
+                Console.WriteLine("Press 1 to freeze die1, 2 to freeze die2 etc.");
+                Console.WriteLine("Press q to confirm your choice");
+                while (true)
+                {
+
+                    string input = Console.ReadLine();
+
+                    if (input == "q") { break; }
+
+                    switch (input)
+                    {
+                        case "q":
+                            break;
+                        case "1":
+                            dieCup.lockDie(0);
+                            Console.WriteLine("Die 1 locked!");
+                            break;
+                        case "2":
+                            dieCup.lockDie(1);
+                            Console.WriteLine("Die 2 locked!");
+                            break;
+                        case "3":
+                            dieCup.lockDie(2);
+                            Console.WriteLine("Die 3 locked!");
+                            break;
+                        case "4":
+                            dieCup.lockDie(3);
+                            Console.WriteLine("Die 4 locked!");
+                            break;
+                        case "5":
+                            dieCup.lockDie(4);
+                            Console.WriteLine("Die 5 locked!");
+                            break;
+
+                    }
+                }
             }
         }
     }
