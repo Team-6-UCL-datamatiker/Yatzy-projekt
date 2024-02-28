8f6c4dd27fdc6b755c42be3c1d0d8ce1433b57dd
Jesper Helstrup <jehe63306@edu.ucl.dk>
2024-02-28 21:49:54 +0100

    Scoreboard now working as intended

--- DieTest/Scoreboard.cs
+++ DieTest/Scoreboard.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace DieTest
 {
@@ -10,10 +11,32 @@ namespace DieTest
     {
         public static void showScoreboard(Player[] players) 
         {
+            //Creating Header
+            Console.Write("Kombination".PadRight(25));
             foreach (var player in players)
             {
+                Console.Write($"{player.name}".PadRight(10));
+            }
+            Console.WriteLine();
+            //Creating Rows
+            foreach (var key in players[0].scores.Keys)
+            {
+                Console.Write(key.PadRight(25));
+
+                foreach (var player in players)
+                {
+                    Console.Write($"{player.scores[key]}".PadRight(10));
+                }
+                Console.WriteLine();
 
             }
+            
+
+            //for (int i = 1; i < 14; i++)
+            //{
+            //    Console.Write();
+            //}
+
 
         }
     }
