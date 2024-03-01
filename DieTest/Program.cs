76d2ade251c7f23467fdd8f550cae9744f223359
Jakob Seeberg <jbse59225@edu.ucl.dk>
2024-03-01 12:56:02 +0100

    -

--- DieTest/Program.cs
+++ DieTest/Program.cs
@@ -22,7 +22,7 @@
                     for (int iRollCounter = 0; iRollCounter < 3; iRollCounter++)
                     {
                         // Rafleanimation:
-                        for (int j = 0; j < 20; j++)
+                        for (int i = 0; i < 20; i++)
                         {
                             dieCup.Roll();
                             crunch.PrintTurn(dieCup, player, playerArray, iRunde, iRollCounter);
